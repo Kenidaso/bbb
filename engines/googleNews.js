79b03f2d97238bd15cb4a79786e1e4e89bce7ede
Chickyky <Chickyky@Fimpluss-MacBook-Pro-3.local>
2020-02-20 04:14:43 +0700

    a

--- engines/googleNews.js
+++ engines/googleNews.js
@@ -670,13 +670,13 @@ const _parse_gg_search = (body) => {
 			publishDate = moment().add(hour * -1, 'h').utcOffset(420).format();
 		}
 		else if (publishDateText.indexOf('hour') > -1) {
-			let _tmp = publishDateText.match(/\d{1,2} hour/)[0];
-			let hour = Number(_tmp.match(/\d{1,2}/)[0]);
+			// let _tmp = publishDateText.match(/\d{1,2} hour/)[0];
+			let hour = Number(publishDateText.match(/\d{1,2}/)[0]);
 			publishDate = moment().add(hour * -1, 'h').utcOffset(420).format();
 		}
 		else if (publishDateText.indexOf('minute') > -1) {
-			let _tmp = publishDateText.match(/\d{1,2} minute/)[0];
-			let minute = Number(_tmp.match(/\d{1,2}/)[0]);
+			// let _tmp = publishDateText.match(/\d{1,2} minute/)[0];
+			let minute = Number(publishDateText.match(/\d{1,2}/)[0]);
 			publishDate = moment().add(minute * -1, 'm').utcOffset(420).format();
 		}
 		else if (publishDateText.indexOf('phút trước') > -1) {
@@ -696,7 +696,6 @@ const _parse_gg_search = (body) => {
 			// 18 Feb 2020
 			let _match = publishDateText.match(/\d{1,2} \w{3,6} \d{2,4}/);
 			let _tmp = _match ? _match[0] : '';
-			_tmp = publishDateText.replace('thg', '').replace(',', '');
 
 			publishDate = moment(_tmp, 'DD MMM YYYY')
 			publishDate = publishDate.isValid() ? publishDate.utcOffset(420).format() : null;
@@ -737,13 +736,13 @@ const _parse_gg_search = (body) => {
 				publishDate = moment().add(hour * -1, 'h').utcOffset(420).format();
 			}
 			else if (publishDateCardText.indexOf('hour') > -1) {
-				let _tmp = publishDateCardText.match(/\d{1,2} hour/)[0];
-				let hour = Number(_tmp.match(/\d{1,2}/)[0]);
+				// let _tmp = publishDateCardText.match(/\d{1,2} hour/)[0];
+				let hour = Number(publishDateCardText.match(/\d{1,2}/)[0]);
 				publishDate = moment().add(hour * -1, 'h').utcOffset(420).format();
 			}
 			else if (publishDateCardText.indexOf('minute') > -1) {
-				let _tmp = publishDateCardText.match(/\d{1,2} minute/)[0];
-				let minute = Number(_tmp.match(/\d{1,2}/)[0]);
+				// let _tmp = publishDateCardText.match(/\d{1,2} minute/)[0];
+				let minute = Number(publishDateCardText.match(/\d{1,2}/)[0]);
 				publishDate = moment().add(minute * -1, 'm').utcOffset(420).format();
 			}
 			else if (publishDateCardText.indexOf('phút trước') > -1) {
@@ -763,7 +762,6 @@ const _parse_gg_search = (body) => {
 				// 18 Feb 2020 In-Depth-BBC Tiếng Việt-16 Feb 2020
 				let _match = publishDateCardText.match(/\d{1,2} \w{3,6} \d{2,4}/);
 				let _tmp = _match ? _match[0] : '';
-				_tmp = publishDateCardText.replace('thg', '').replace(',', '');
 
 				publishDate = moment(_tmp, 'DD MMM YYYY')
 				publishDate = publishDate.isValid() ? publishDate.utcOffset(420).format() : null;
