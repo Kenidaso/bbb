3b9e0cb19fe2b8225729c89789916da28cff0ffe
QUYEN LE <quyenle@cpe-172-118-0-148.socal.res.rr.com>
2022-08-03 15:58:20 +0700

    update ecosystem.config for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   apps : [{
     name: 'news-api',
-    script: 'keystone.js'
+    script: 'keystone.js',
+    out_file: '/logs/api'
   }, {
     name: 'rss',
-    script: 'npm run rss'
+    script: 'npm run rss',
+    out_file: '/logs/worker_rss'
   }, {
     name: 'task_queue',
-    script: 'npm run task_queue'
+    script: 'npm run task_queue',
+    out_file: '/logs/worker_task'
   }]
 };
