1678e10da1ccc37395dc94d3a2e1d279a71526c0
QUYEN LE <quyenle@cpe-172-118-0-177.socal.res.rr.com>
2023-04-27 16:55:09 +0700

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,15 @@ module.exports = {
     name: 'news-api',
     script: 'keystone.js',
     out_file: '/root/.pm2/logs/api.log'
-  }, {
-    name: 'rss',
-    script: 'npm run rss',
-    out_file: '/root/.pm2/logs/worker_rss.log'
-  }, {
-    name: 'task_queue',
-    script: 'npm run task_queue',
-    out_file: '/root/.pm2/logs/worker_task.log'
-  }]
+  }
+  // {
+  //   name: 'rss',
+  //   script: 'npm run rss',
+  //   out_file: '/root/.pm2/logs/worker_rss.log'
+  // }, {
+  //   name: 'task_queue',
+  //   script: 'npm run task_queue',
+  //   out_file: '/root/.pm2/logs/worker_task.log'
+  // }
+  ]
 };
